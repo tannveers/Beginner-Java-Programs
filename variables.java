dbe735abe9963020344b4db33d57b8828355a142
Tanveer Singh <tanveer9812@gmail.com>
2020-05-21 00:56:10 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,20 @@
+public class index {
+
+	public static void main(String[] args) {
+		int a=7;
+		float b=2.5565656f;
+		double x=3.414141;
+		boolean val = true;
+		char c = 'T';
+		String wow= "wow";
+		
+		//Many other data types like byte, short, long int, Classes, interface, arrays etc...
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(x);
+		System.out.println(val);
+		System.out.println(c);
+		System.out.println(wow);
+	}
+}
