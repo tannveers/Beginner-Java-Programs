bc778e40b4c0af05d4b3cbbb51930aa63f60221e
Tanveer Singh <tanveer9812@gmail.com>
2020-05-21 01:15:30 +0530

    Create if_else.java

---
+++ if_else.java
@@ -0,0 +1,11 @@
+public class index {
+	public static void main(String[] args) {
+		int x=10;
+		if(x%2 == 0) {
+			System.out.println("Even number");
+		}
+		else {
+			System.out.println("odd number");
+		}
+	}
+}
