2e31bbe4ab7f3c2d466d9f7b34eea99f308de0e9
Tanveer Singh <tanveer9812@gmail.com>
2020-05-21 01:03:01 +0530

    input
    Program to simulate input from user

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class index {
+
+	public static void main(String[] args) {
+	System.out.println("Enter the word you want to display");
+	Scanner input = new Scanner(System.in);
+	System.out.println(input.nextLine());
+}
+}
