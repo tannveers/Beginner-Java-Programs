6fc7f7af9ec5710f792aa1eaf9e3af71e8c85a86
Tanveer Singh <tanveer9812@gmail.com>
2020-05-21 01:13:46 +0530

    increment
    how ++ and -- operator works?

---
+++ increment.java
@@ -0,0 +1,10 @@
+public class index {
+	public static void main(String[] args) {
+		int x=10;
+		System.out.println(x++); // print and increment
+		System.out.println(++x); // increment and print
+		System.out.println(x--); // print and decrement
+		System.out.println(--x); // decrement and print
+		System.out.println(x);   // print as it is
+	}
+}
