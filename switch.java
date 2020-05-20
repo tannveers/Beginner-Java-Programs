9dcf3a7162c56684940a956a062e75a7e23880e5
Tanveer Singh <tanveer9812@gmail.com>
2020-05-21 01:22:21 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,19 @@
+public class index {
+	public static void main(String[] args) {
+		int a=20;
+		switch(a) {
+		case 10:
+			System.out.println(" case 10 worked ");
+			break;
+		case 15:
+			System.out.println(" case 15 worked ");
+			break;
+		case 20:
+			System.out.println(" case 20 worked ");
+			break;
+			
+		default:
+			System.out.println("Default worked!");
+		}
+}
+}
