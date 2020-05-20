f9f44e479f289c82280bd43a2b4f0bcb97123b18
Tanveer Singh <tanveer9812@gmail.com>
2020-05-21 01:27:58 +0530

    Create while_loop.java

---
+++ while_loop.java
@@ -0,0 +1,9 @@
+public class index {
+	public static void main(String[] args) {
+		int a=20;
+		while(a>0) { // A while loop keeps printing the values assigned to he conditional variable till it's not met
+			// As in the following case, keeps printing the value of a till it's 0;
+			System.out.println(a--);
+		}
+}
+}
